c5bc21424023774eac43615b0c19b2763b8798ab
jojogoser3 <55130268+jojogoser3@users.noreply.github.com>
2020-05-29 14:02:25 +0200

    template made and handlebars configured

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
+const db = require('./queries');
+const handlebars = require('express-handlebars');
+
+db.getChats.then(res => console.log(res)).catch(err => console.log(err));
+
+app.use(express.static(__dirname + '/public'));
+
+//instead of app.set('view engine', 'handlebars'); 
+app.set('view engine', 'hbs');
+
+app.engine('hbs', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    //new configuration parameter
+    extname: 'hbs'
+}));
+
+app.get('/', (req, res) => {
+//Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+    res.render('login', {layout : 'index'});
+});
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/views/main.html');
 })
 
-app.get('/javascript', (req, res) => {
-    res.sendFile(__dirname + '/public/javascript.html');
-})
+// app.get('/javascript', (req, res) => {
+//     res.sendFile(__dirname + '/public/javascript.html');
+// })
 
-app.get('/css', (req, res) => {
-    res.sendFile(__dirname + '/public/css.html');
-})
+// app.get('/css', (req, res) => {
+//     res.sendFile(__dirname + '/public/css.html');
+// })
 
-app.get('/swift', (req, res) => {
-    res.sendFile(__dirname + '/public/swift.html');
-})
+// app.get('/swift', (req, res) => {
+//     res.sendFile(__dirname + '/public/swift.html');
+// })
 
 // tech namespace
 const tech = io.of('/tech');
---
+++ queries.js
@@ -0,0 +1,33 @@
+const pg = require('pg');
+const process_db = require('dotenv').config();
+const db_url = process.env.DATABASE_URL || process_db.parsed.DB_URL;
+const client = new pg.Client({
+    connectionString: db_url,
+    ssl: { rejectUnauthorized: false }
+});
+client.connect();
+
+const insertChats = (request) => {
+    const data = request;
+
+    client.query('INSERT INTO chats (user_name, room, chat_text, date_time) VALUES ($1, $2, $3, NOW())',
+    [data.name, data.room, data.text], (error, results) => {
+        if(error){
+            throw error;
+        }
+        console.log(`Chat added to room: ${data.room}`);
+    })
+}
+
+const getChats = new Promise((resolve, reject) => {
+    client.query('SELECT * FROM chats')
+    .then(results => {
+        resolve(results.rows);
+    })
+    .catch(e => console.error(e.stack));
+})
+
+module.exports = {
+    getChats,
+    insertChats
+}
